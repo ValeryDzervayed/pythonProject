c5b42323dcd1b201fa9d2a6dee6088ad02f3c353
ValeryDzervayed <dervoed.brest@gmail.com>
2021-03-28 01:24:21 +0300

    SQLite

---
+++ SQLite/SQLite_3.py
@@ -0,0 +1,10 @@
+# python-scripts.com
+import sqlite3
+
+conn = sqlite3.connect("mydatabase.db")
+cursor = conn.cursor()
+
+sql = "DELETE FROM albums WHERE artist = 'John Doe'"
+
+cursor.execute(sql)
+conn.commit()
